d9f5ef2b22eb77f8321b7e55fc7251121ad31c9e
Mitch MacKenzie <mitchmac@gmail.com>
2012-04-05 10:19:58 -0300

    Initial commit

---
+++ templates/fjm_merce_audio_object.tpl.php
@@ -0,0 +1,8 @@
+<video id="flashContent">
+    <a href="http://www.macromedia.com/go/getflashplayer">Get the Flash Player</a> to see this player.
+</video>
+
+<div id="audio-mods">
+    <?php print t('Description'); ?>
+    <?php print $mods_data; ?>
+</div>
---
+++ templates/fjm_merce_landing_page.tpl.php
@@ -0,0 +1,7 @@
+<div id="merce-landing-page">
+	<?php print $menu; ?>
+	<?php print $carousel; ?>
+	<?php print $bio; ?>
+	<?php print $browse; ?>
+	<?php print $search_form; ?>
+</div>
