f850f4b8628e2872651602e75211166924f766eb
Danny Joris <danny_joris@yahoo.co.uk>
2012-04-09 17:12:45 -0300

    general layout for the front page

--- templates/fjm_merce_landing_page.tpl.php
+++ templates/fjm_merce_landing_page.tpl.php
@@ -1,7 +1,29 @@
 <div id="merce-landing-page">
-	<?php print $menu; ?>
-	<?php print $carousel; ?>
-	<?php print $bio; ?>
-	<?php print $browse; ?>
-	<?php print $search_form; ?>
+	<div id="front-wrapper-top">
+	  <div id="front-title"></div>
+	  <div id="front-portrait"></div>
+	  <div id="front-bio">
+	    <?php print $bio; ?>
+	  </div>
+	  <div id="front-fjm-menu">
+	    <?php print $menu; ?>
+	  </div>
+	  <div id="front-carousel">
+      <?php print $carousel; ?>
+	  </div>
+	</div>
+	<div id="front-wrapper-bottom">
+	  <div id="front-browse">
+	    <h2 class="front-title"><?php print t('Browse'); ?></h2>
+	    <div id="front-browse-wrapper">
+	      <?php print $browse; ?>
+	    </div>
+	  </div>
+	  <div id="front-search-form">
+	    <h2 class="front-title"><?php print t('Search'); ?></h2>
+	    <div id="front-search-wrapper">
+	      <?php print $search_form; ?>
+	    </div>
+	  </div>
+	</div>
 </div>
