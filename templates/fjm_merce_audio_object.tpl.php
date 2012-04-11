feb3137f5ae8743eaceea966db315073e13db9e6
Danny Joris <danny_joris@yahoo.co.uk>
2012-04-11 13:13:43 -0300

    markup changes audio object

--- templates/fjm_merce_audio_object.tpl.php
+++ templates/fjm_merce_audio_object.tpl.php
@@ -1,8 +1,22 @@
-<video id="flashContent">
-    <a href="http://www.macromedia.com/go/getflashplayer">Get the Flash Player</a> to see this player.
-</video>
-
-<div id="audio-mods">
-    <?php print t('Description'); ?>
-    <?php print $mods_data; ?>
+<div id="audio-object">
+  <div class="region-left">
+    <video id="flashContent">
+      <a href="http://www.macromedia.com/go/getflashplayer">Get the Flash Player</a> to see this player.
+    </video>
+  </div>
+  <div class="region-right">
+    <div id="audio-mods">
+      <div class="metadata-tab">
+        <?php print t('Description'); ?>
+      </div>
+      <div class="metadata-wrapper">
+        <div class="metadata-wrapper-inner">
+          <div class="metadata-top">
+            <?php print 'cordoba (collection obj title)'; ?>
+          </div>
+          <?php print $mods_data; ?>  
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
